f8bb1bad8bd925d6634b8377a5460c7172e9adb6
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2018-10-31 19:01:40 +1100

    Convert hex to base64

---
+++ matasano/basics_test.go
@@ -0,0 +1,19 @@
+package matasano
+
+import (
+	"testing"
+)
+
+func TestHexToBase64(t *testing.T) {
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	should := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+	result, err := HexToBase64(input)
+
+	if err != nil {
+		t.Error("Error occured during conversion")
+	}
+	if result != should {
+		t.Error("Expected ", should, ", not ", result)
+	}
+}
